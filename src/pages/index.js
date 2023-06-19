a954af4246ad8c5ea4136ae4ece25fb5524db1f1
brendanmmmkay <brendanmmmkay@gmail.com>
2023-06-19 14:23:15 -0700

    Index Update 1 Jul19

--- src/pages/index.js
+++ src/pages/index.js
@@ -34,28 +34,33 @@ export default function Home() {
       className="absolute z-1 text-gray-900 top-0 left-0 w-full bg-regal-gray lg:px-48 px-16"
     >
       <main>
-        <div className='my-96 p-1'>
+        <div className='my-48 p-1'>
           <m.h1
             animate={{ y: 0 }}
             initial={{ y: "100%" }}
             transition={{ delay: 0.5, duration: 0.5 }}
-            className='text-4xl text-left lg:text-9x1 font-ArgentItalic text-white'>I weave business value with compelling visual storytelling toward impactful design that solves problems for people.
+            className='text-4xl text-left lg:text-9x1 font-ArgentItalic text-white xl:w-5/6'>I weave business value with compelling visual storytelling toward impactful design that solves problems for people.
           </m.h1>
         </div>
 
         <div className="mt-40">
+        <Link href={"/contact"}>
           <h1 className='text-5xl font-ArgentItalic text-white pb-5'>In Search Of</h1>
+          </Link>
         </div>
         <div class="grid grid-cols-1 divide-y divide-linear-gray">
           <div></div>
           <div class="grid lg:grid-cols-3 sm:grid-cols-1 gap-6">
             <div className="lg:pt-5">           
+            <Link href={"/contact"}>
               <Image
               src={test1}
               width={500}
               height={200} />
+</Link>
               </div>
             <div className="lg:pt-5">
+            <Link href={"/contact"}>
             <MuxPlayer
                 streamType="on-demand"
                 playbackId="oWqKBxCME02obOSNxtb01T801ew3tcgwwOp5502l7BihPyc"
@@ -66,6 +71,7 @@ export default function Home() {
                 autoplay="muted"
                 loop
             />
+            </Link>
             </div>
             <div className="lg:pt-5">
             <Link href={"/contact"}>
@@ -131,7 +137,7 @@ export default function Home() {
 
 
         <div className="mt-20">
-          <h1 className='text-5xl font-ArgentItalic text-white pb-5'>Bosch Courier Connect</h1>
+          <h1 className='text-5xl font-ArgentItalic text-white pb-5'>Bosch Courier Connect - Coming Soon</h1>
         </div>
         <div class="grid grid-cols-1 divide-y divide-linear-gray">
           <div></div>
@@ -167,7 +173,7 @@ export default function Home() {
 
 
         <div className="mt-20">
-          <h1 className='text-5xl font-ArgentItalic text-white pb-5'>Moment Energy</h1>
+          <h1 className='text-5xl font-ArgentItalic text-white pb-5'>Moment Energy - Coming Soon</h1>
         </div>
         <div class="grid grid-cols-1 divide-y divide-linear-gray">
           <div></div>
@@ -201,33 +207,31 @@ export default function Home() {
 
 
 
-        <div className="flex justify-between">
-          <Link href={"/contact"}>
-            <h2>Design</h2>
-          </Link>
-          <div>
-            <h2>hi</h2>
-          </div>
-          <h2>Company</h2>
-          <h2>2022</h2>
-          <div class="grid grid-cols-2 gap-6">
-            <div>
-              uh
+        <div className="flex pt-40 gap-40">
+                <div className="lg:text-2xl text-base text-white">
+                    
+                        <h4>Find me:</h4>
+                    
+                </div>
+                <div className="lg:text-6xl text-2xl underline text-white">
+                    <ul>
+                        <div className="overflow-hidden">
+                        <Link href={"mailto:bpmckay@sfu.ca"}>
+                            <li className="pb-4">bpmckay@sfu.ca</li>
+                            </Link>
+                            <Link href={"https://www.linkedin.com/in/brendan-mckay/"}>
+                            <li className="pb-4">Linkedin</li>
+                            </Link>
+                            <Link href={"https://read.cv/bedthyme/"}>
+                            <li className="pb-4">Read.cv</li>
+                            </Link>
+                            <Link href={"https://medium.com/@brendanmckaydesign"}>
+                            <li className="pb-12">Medium</li>
+                            </Link>
+                        </div>
+                    </ul>
+                </div>
             </div>
-            <div>Box 2</div>
-            <div>Box 3</div>
-            <div>Box 4</div>
-          </div>
-          <div class="flex flex-col-reverse">
-            <div>01</div>
-            <div>02</div>
-            <div>03</div>
-          </div>
-        </div>
-        <div>
-          <h3>Canadian designs to make you coom.</h3>
-          <h3>click contact for cool trans girl to appear.</h3>
-        </div>
 
 
 
