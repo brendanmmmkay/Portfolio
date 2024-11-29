d3f84c492a1bab9917cd05ac34c62b094f16abd4
brendanmmmkay <brendanmmmkay@gmail.com>
2024-11-29 01:12:57 -0800

    fixing previous commit

--- src/pages/moment.js
+++ src/pages/moment.js
@@ -554,7 +554,6 @@ export default function Bosch() {
 
                 </div>
 
-                <Image className="block md:hidden z-0 absolute pt-10 right-5/6 mb-10 scale-50" src={house} />
 
                 <div className="rounded-md overflow-hidden">
                     {/* <Image
